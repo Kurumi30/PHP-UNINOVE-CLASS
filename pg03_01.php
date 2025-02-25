de45e8d87395aad7c7ec79cb4427f4ada5e9ad6b
Fernando Zhu <39763262-fernandoshengxi@users.noreply.replit.com>
2025-02-25 00:07:26 +0000

    feat: add pg03_01.php

---
+++ pg03_01.php
@@ -0,0 +1,18 @@
+<?php
+  $idade = 25;
+  $nota = 7.8;
+  $cep = "03512123";
+  $nome = "Fernando";
+
+  // echo "$nome, você tem $idade anos e sua nota foi $nota <hr>";
+
+  // var_dump($idade);
+  // var_dump($nota);
+  // var_dump($nome);
+
+  // echo "CEP: $cep";
+
+  // decimal: 01234567  8   9
+  // octal: 01234567   10   11
+  // binário: 01 10
+?>

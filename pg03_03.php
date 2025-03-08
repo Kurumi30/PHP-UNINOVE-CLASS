1d4eb327d834c25e1ac27f393735f7f2d73ed9d2
Fernando Zhu <39763262-fernandoshengxi@users.noreply.replit.com>
2025-03-08 21:41:02 +0000

    feat: add pg03_03.php

---
+++ pg03_03.php
@@ -0,0 +1,12 @@
+<?php
+  $nome = "Claudete";
+  $sobrenome = 'Maria';
+  $cor = "red";
+
+  echo $nome ." ".$sobrenome."<br>";
+  echo "$nome $sobrenome<br>";
+  echo '$nome $sobrenome<br>';
+  echo "<h1 style='color: $cor;'>Texto em h1 - $nome</h1>";
+?>
+
+<!-- h1 style="color: blue">Texto em h1</h1>

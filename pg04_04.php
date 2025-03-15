f78e559e695e05652b1427b3282ee47a055679fe
Fernando Zhu <39763262-fernandoshengxi@users.noreply.replit.com>
2025-03-15 14:18:21 +0000

    feat: add pg04_04.php

---
+++ pg04_04.php
@@ -0,0 +1,18 @@
+<?php
+  $x = 100;
+
+  echo "x = ".$x++;
+  echo "<br>x final = ".$x;
+
+  # O operador ++ incrementa 1 primeiro
+  # A variável é incrementada depois que a instrução for executada
+
+  echo "<hr>";
+
+  $i = 10;
+  $i++;
+  ++$i;
+  $i--;
+
+  echo "i = $i"; // Vai valer 11
+?>

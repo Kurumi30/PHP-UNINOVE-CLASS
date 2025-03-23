2e50008cef622eab66141b4e9f948fab4de88423
Fernando Zhu <96603684+Kurumi30@users.noreply.github.com>
2025-03-23 16:32:20 +0000

    feat: add page05_02.php

---
+++ pg05_02.php
@@ -0,0 +1,10 @@
+<?php
+  $a = 10;
+  $b = 10;
+  $c = 11;
+
+  echo "Menor: ".($a < $b)."<br>"; // 
+  echo "Menor ou igual: ".($a <= $b)."<br>"; // 1
+  echo "Maior: ".($c > $b)."<br>"; // 1
+  echo "Maior ou igual: ".($c >= $b)."<br>"; // 1
+?>

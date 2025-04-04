e7635a13f8dd0263531fe67e4a93a8f26dd14142
Fernando Zhu <96603684+Kurumi30@users.noreply.github.com>
2025-04-04 02:07:50 +0000

    wip

---
+++ index-2.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php
+			$_nome = "João Antonio";
+			$nome = "Alfredo Maciel";
+			$valor = 123456;
+			$nota = 7.5;
+
+			echo $_nome;
+			$nota = 8.5;
+			echo "<br/>Nota $nota";
+		?>
+	</body>
+</html>
